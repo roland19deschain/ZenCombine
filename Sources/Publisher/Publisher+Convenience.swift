93822a48a16cb9c758c677fc760b7c25b9cbed6c
Alexey Roik <roland19deschain@gmail.com>
2022-08-06 20:40:56 +0300

    Update 'Publisher+Convenience' extensions

--- Sources/Publisher/Publisher+Convenience.swift
+++ Sources/Publisher/Publisher+Convenience.swift
@@ -2,6 +2,14 @@ import Combine
 
 public extension Publisher {
 	
+	/// Catches the errors and immediately completes the publishes instead.
+	var dropError: AnyPublisher<Output, Never> {
+		`catch` { _ in
+			Empty<Output, Never>()
+		}.eraseToAnyPublisher()
+	}
+	
+	/// Maps the value to `Void`.
 	func mapToVoid() -> Publishers.Map<Self, Void> {
 		map { _ in () }
 	}
